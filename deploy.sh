3e153aad5c7f8991015be84bd0df9794ea67de27
Rafał Wrzeszcz <rafal.wrzeszcz@wrzasq.pl>
2024-11-07 23:46:52 +0100

    WrzasqPlCform macro deployment.

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,15 @@ echo "Deploying for ${env_name} @ ${customer_name}"
 
 stack_prefix="${customer_name}-${env_name}"
 
+# shared stacks
+aws cloudformation deploy \
+    --stack-name "${env_name}-devops" \
+    --template-file "cloudformation/devops.yaml" \
+    --tags stage:name=${env_name} \
+    --parameter-overrides WrzasqPlCformVersion=1.4.0 \
+    --capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_IAM
+
+# per-customer stacks (in case of multi-customer deployment)
 aws cloudformation deploy \
     --stack-name "${stack_prefix}-storage" \
     --template-file "cloudformation/storage.yaml" \
